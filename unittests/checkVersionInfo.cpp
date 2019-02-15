991e4773f1a154bb2a241d4d22b2eca7ae3f8ab6
Mikhail Ronzhin <ronme@mail.ru>
2019-02-15 21:50:03 +0600

    fix tests

--- unittests/checkVersionInfo.cpp
+++ unittests/checkVersionInfo.cpp
@@ -1,50 +1,37 @@
 
-
+#include <cstdlib>
+#include <ctime>
 #include "../src/updater.h"
+
+bool compareVersion(const VersionInfo &vi, const std::wstring &val)
+{
+    if (vi.ToString().compare(val) != 0)
+    {
+        wprintf(L"%ls %ls\n", vi.ToString().c_str(), val.c_str());
+        return false;
+    }
+    return true;
+}
 
 int main()
 {
     VersionInfo vi;
     auto v = vi.ToString();
 
-    if (vi.ToString().compare(L"0.0.0.0") != 0)
+    if (!compareVersion(vi, L"0.0.0.0"))
     {
         return EXIT_FAILURE;
     }
 
+    std::srand(unsigned(std::time(0)));
     for (int i = 1; i < 10000; ++i)
     {
-        vi.high = i;
-        if (vi.ToString().compare(std::to_wstring(i) + L".0.0.0") != 0)
+        vi.high = std::rand();
+        vi.low = std::rand();
+        vi.release = std::rand();
+        vi.build = std::rand();
+        if (!compareVersion(vi, std::to_wstring(vi.high) + L"." + std::to_wstring(vi.low) + L"." + std::to_wstring(vi.release) + L"." + std::to_wstring(vi.build)))
         {
-            wprintf(L"%ls \n", vi.ToString().c_str());
-            return EXIT_FAILURE;
-        }
-
-        vi.low = i;
-        if (vi.ToString().compare(std::to_wstring(vi.high) + L"." +
-                                  std::to_wstring(i) + L".0.0") != 0)
-        {
-            wprintf(L"%ls \n", vi.ToString().c_str());
-            return EXIT_FAILURE;
-        }
-
-        vi.release = i;
-        if (vi.ToString().compare(std::to_wstring(vi.high) + L"." +
-                                  std::to_wstring(vi.low) + L"." +
-                                  std::to_wstring(i) + L".0") != 0)
-        {
-            wprintf(L"%ls \n", vi.ToString().c_str());
-            return EXIT_FAILURE;
-        }
-
-        vi.build = i;
-        if (vi.ToString().compare(std::to_wstring(vi.high) + L"." +
-                                  std::to_wstring(vi.low) + L"." +
-                                  std::to_wstring(vi.release) + L"." +
-                                  std::to_wstring(i)) != 0)
-        {
-            wprintf(L"%ls \n", vi.ToString().c_str());
             return EXIT_FAILURE;
         }
     }
