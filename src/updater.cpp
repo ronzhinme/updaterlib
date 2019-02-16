2d7b932eb10244d0a81fffc1845701b373f7b55c
Mikhail Ronzhin <ronme@mail.ru>
2019-02-16 14:21:33 +0600

    add test

--- src/updater.cpp
+++ src/updater.cpp
@@ -1,13 +1,32 @@
 
 #include <stdio.h>
+#include <wchar.h>
 #include "updater.h"
 
-const std::wstring VersionInfo::ToString() const
+VersionInfo::VersionInfo(const std::wstring &val)
+{
+    auto wstr = val;
+    for (std::wstring::iterator i = wstr.begin(); i != wstr.end(); ++i)
+    {
+        if (*i == L'.')
+        {
+            wstr = wstr.replace(i, i + 1, L" ");
+        }
+    }
+    
+    wchar_t *pEnd;
+    high = wcstoul(wstr.c_str(), &pEnd, 10);
+    low = wcstoul(pEnd, &pEnd, 10);
+    release = wcstoul(pEnd, &pEnd, 10);
+    build = wcstoul(pEnd, &pEnd, 10);
+}
+
+const std::wstring VersionInfo::toString() const
 {
     return std::to_wstring(high) + L"." + std::to_wstring(low) + L"." + std::to_wstring(release) + L"." + std::to_wstring(build);
 }
 
 const std::wstring Updater::getCurrentVersion() const
 {
-    return m_verInfo.ToString();
+    return m_verInfo.toString();
 }
