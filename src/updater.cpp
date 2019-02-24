13e9636418d942a3648a5a28d42399044e022ad3
Mikhail Ronzhin <ronme@mail.ru>
2019-02-24 11:53:54 +0600

    add auto update interval

--- src/updater.cpp
+++ src/updater.cpp
@@ -24,7 +24,24 @@ std::wstring Updater::getVersionInfoUrl() const
     return m_infoUrl.c_str();
 }
 
+unsigned long Updater::getAutoUpdateInterval() const
+{
+    return m_autoUpdateInterval;
+}
+
+void Updater::setAutoUpdateInterval(unsigned long hours)
+{
+    m_autoUpdateInterval = hours;
+}
+
 void Updater::checkForUpdate() const
 {
-
+    // download signature (dSign)
+    // download xml (xmlInfo)
+    // calculate xml signature (cSign)
+    // compare cSign and dSign
+    // if diff return with errCode
+    // parse xml
+    // check if xml contains info about new version
+    // return with okCode
 }
--- src/updater.h
+++ src/updater.h
@@ -18,9 +18,12 @@ public:
     std::wstring getVersionInfoUrl() const;
     void setVersionInfoUrl(const wchar_t* url);
     void checkForUpdate() const;
+    unsigned long getAutoUpdateInterval() const;
+    void setAutoUpdateInterval(unsigned long hours);
 private:
     std::wstring m_infoUrl;
     VersionInfo m_verInfo; 
+    unsigned long m_autoUpdateInterval;
 };
 
 #endif //UPDATER_H
