579c44d09820249a36e2581a7b9be6928f36c5a9
m <ronme@mail.ru>
2019-09-21 14:17:55 +0600

    fix updaterInterface

--- src/updaterInterface.h
+++ src/updaterInterface.h
@@ -17,7 +17,6 @@ extern "C" _declspec(dllexport)  void getInfoUrl(UPDATER_PTR updater, char* val,
 extern "C" _declspec(dllexport)  void setChannel(UPDATER_PTR updater, const char* val, size_t len);
 extern "C" _declspec(dllexport)  void getChannel(UPDATER_PTR updater, char* val, size_t len);
 extern "C" _declspec(dllexport)  void setAutoUpdateInterval(UPDATER_PTR updater, size_t hours);
-extern "C" _declspec(dllexport)  void setAutoUpdateInterval(UPDATER_PTR updater, size_t hours);
 extern "C" _declspec(dllexport)  void downloadInfo(UPDATER_PTR updater);
 extern "C" _declspec(dllexport)  void downloadUpdate(UPDATER_PTR updater);
 extern "C" _declspec(dllexport)  void checkSignature(UPDATER_PTR updater);
@@ -28,12 +27,11 @@ UPDATER_PTR getUpdaterInstance();
 void setCurrentVersion(UPDATER_PTR updater, const char* val, size_t len);
 void getCurrentVersion(UPDATER_PTR updater, char* val, size_t len);
 size_t getNextVersionAttribute(UPDATER_PTR updater, char * out, size_t outLen, const char* attribute, size_t attributeLen);
-void findUpdateVersion(UPDATER_PTR updater);
+void findNextVersion(UPDATER_PTR updater);
 void setInfoUrl(UPDATER_PTR updater, const char* val, size_t len);
 void getInfoUrl(UPDATER_PTR updater, char* val, size_t len);
 void setChannel(UPDATER_PTR updater, const char* val, size_t len);
 void getChannel(UPDATER_PTR updater, char* val, size_t len);
-void setAutoUpdateInterval(UPDATER_PTR updater, size_t hours);
 void setAutoUpdateInterval(UPDATER_PTR updater, size_t hours);
 void downloadInfo(UPDATER_PTR updater);
 void downloadUpdate(UPDATER_PTR updater);
@@ -43,4 +41,4 @@ void setOperationResultEvent(UPDATER_PTR updater, OperationResultFunction handle
 #endif
 
 
-#endif // MTREPORTLIBH
+#endif // UPDATER_INTERFACE_H
