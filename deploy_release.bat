e7e6ca4b0e89106d0c34b821561d81596e75b41b
Ronzhin Mikhail <ronme@mail.ru>
2020-01-02 09:31:38 +0000

    Resolve "Refactor: deploy with dependencies"

--- deploy_release.bat
+++ deploy_release.bat
@@ -7,3 +7,5 @@ set description=%3
 curl -X DELETE -H "PRIVATE-TOKEN: %token%" -d tag_name=%tagName% --url "https://gitlab.com/api/v4/projects/12425924/releases/%tagName%"
 curl -X POST -H "PRIVATE-TOKEN: %token%" -d name=%tagName% -d description=%description% -d tag_name=%tagName% --url "https://gitlab.com/api/v4/projects/12425924/releases"
 curl -X POST -H "PRIVATE-TOKEN: %token%" -d name="linux" -d url="https://gitlab.com/ronme/appupdaterlib/-/jobs/artifacts/%tagName%/download?job=linux_build" --url "https://gitlab.com/api/v4/projects/12425924/releases/%tagName%/assets/links"
+curl -X POST -H "PRIVATE-TOKEN: %token%" -d name="windows" -d url="https://gitlab.com/ronme/appupdaterlib/-/jobs/artifacts/%tagName%/download?job=win_build" --url "https://gitlab.com/api/v4/projects/12425924/releases/%tagName%/assets/links"
+curl -X POST -H "PRIVATE-TOKEN: %token%" -d name="mac" -d url="https://gitlab.com/ronme/appupdaterlib/-/jobs/artifacts/%tagName%/download?job=mac_build" --url "https://gitlab.com/api/v4/projects/12425924/releases/%tagName%/assets/links"
